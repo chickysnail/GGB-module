28f5412505004a0aad17ed0907a7ea9d8a19c70c
Artem Tronin <artem.m.tronin@gmail.com>
2022-09-11 09:02:19 +0300

    small changes in naming after n-week gap in project

--- main.py
+++ main.py
@@ -8,22 +8,18 @@ functionNames = {
     "point": r"(point|pnt)",
     "line": r"(line|ln)",
 }
+
+# sub patterns
 name = r"([a-zA-Z]\w*)"
 coordinates = r"(\d* \d*)"
 
-class Patterns:
+class fun:
     # point (Name) ([x y])
     point = re.compile(rf"{name}? ?{functionNames['point']} ?{coordinates}?")
     # line (Name) ([[x y]|Point]|[[x y]|Point])
     line = re.compile(rf"{name}? ?{functionNames['line']} ?({coordinates}|{name}&)")
 
-# patterns = {
-#     # point (Name) ([x y])
-#     "point": re.compile(rf"{name}? ?{functionNames['point']} ?{coordinates}?")
-#     # line (Name) ([[x y]|Point]|[[x y]|Point])
-#     "line"
-# }
 
 for i, row in enumerate(open('inputExample.txt')):
-    for match in re.finditer(Patterns.point, row):
+    for match in re.finditer(fun.point, row):
         print('Found on line %s: %s' % (i+1, match.groups()))
