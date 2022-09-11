9f7547a62b0c2c431c984aaa57d00473c3cf7596
Artem Tronin <artem.m.tronin@gmail.com>
2022-09-11 11:50:50 +0300

    added Line function grouping

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 import re
 
 
-# functions: point
+# e for execute
+def point(values):
+    name = values["name"]
+    coords: list = [0,0] if values["coords"] == None else values["coords"].split()
+    
+    out=""
+    if values["name"] is not None:
+        out = f'{values["name"]}='
+    out += f'({coords[0]}, {coords[1]})'
+    
+    return out
+
+def line(values):
+    return 
+
+# functions: point, line
 # TODO: point on, line
 
-functionNames = {
-    "point": r"(point|pnt)",
-    "line": r"(line|ln)",
+funNames = {
+    point: r"(?:point|p)",
+    line: r"(?:line|ln)",
 }
 
+
 # sub patterns
-name = r"([a-zA-Z]\w*)"
-coordinates = r"(\d* \d*)"
+name = r"(?:[a-zA-Z]\w*)"
+coordinates = r"(?:\d* \d*)"
 
 class fun:
     # point (Name) ([x y])
-    point = re.compile(rf"{name}? ?{functionNames['point']} ?{coordinates}?")
+    point = re.compile(rf"({name})? ?({funNames[point]}) ?({coordinates})?")
     # line (Name) ([[x y]|Point]|[[x y]|Point])
-    line = re.compile(rf"{name}? ?{functionNames['line']} ?({coordinates}|{name}&)")
+    # "(?:((?:\d* \d*)|(?:[a-zA-Z]\w*)) ((?:\d* \d*)|(?:[a-zA-Z]\w*)))"
+    line = re.compile(rf"({name})? ?({funNames[line]}) ?(?:({coordinates}|{name}) ({coordinates}|{name}))")
 
 
 for i, row in enumerate(open('inputExample.txt')):
+    print(f"-{i+1}-")
+
+    for match in re.finditer(fun.line, row):
+        print('Found on line %s: %s' % (i+1, match.groups()))
+
     for match in re.finditer(fun.point, row):
+        cutcmd = match.groups()
+        if re.search(cutcmd[1], funNames[point]):
+            ggbcomand = point({"name": cutcmd[0], "coords": cutcmd[2]})
+        print(ggbcomand)
         print('Found on line %s: %s' % (i+1, match.groups()))
