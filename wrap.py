f7a5d888c9f4e09878aad58142767bc673d2d2e1
Artem Tronin <artem.m.tronin@gmail.com>
2022-09-16 10:19:50 +0300

    V1.0 added wrapper. can work independently using pyinstallet

--- wrap.py
+++ wrap.py
@@ -1,11 +1,13 @@
+from distutils.sysconfig import customize_compiler
 import transformer
-import pyperclip3 as pc
+import pyperclip as pc
 
-file = "inputExample.txt"
-with open(file) as f:
-    text = "".join(f.readlines())
 
 text = "u line 1 2 B"
+customInput = True
+if customInput:
+    text = input()
+
 for i in transformer.transformInput(text):
     print(i)
     pc.copy(i)
